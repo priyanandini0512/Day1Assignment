4c381503731a8c153f1ed93fd2bd62c710674ff2
priyanandini0512 <22b01a45b3@svecw.edu.in>
2024-07-22 10:45:37 -0700

    TestCommit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import  ReactDOM  from "react-dom/client";
+import F6 from "./reviewpart"
+const rootElement=document.getElementById("root");
+const rootref = ReactDOM.createRoot(rootElement);
+rootref.render(<F6/>)
